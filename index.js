c05ff43b6977e4835d449cf82bbb54ca13436fce
codePlaceOfficial <codeplace@yeah.net>
2022-02-21 11:10:18 +0800

    暂时将工作文件夹固定

--- index.js
+++ index.js
@@ -37,40 +37,45 @@ const deleteFolderRecursive = function (path) {
 
 const sandboxManager = new SandboxManager(50);
 io.on('connection', (socket) => {
-    let dirName = cryptoRandomString({ length: 14 });
-    let workPath = path.join(__dirname, "code", dirName)
-    fs.mkdir(workPath, () => {
-        sandboxManager.createSandbox(workPath).then(sandbox => {
-            // terminal
-            sandbox.container.getCmdStream().then(stream => {
-                stream.on('data', (chunk) => {
-                    socket.emit("data", chunk.toString());
-                });
-                socket.on("write", data => {
-                    stream.write(data);
-                })
+    // temp
+    // let dirName = cryptoRandomString({ length: 14 });
+    // let workPath = path.join(__dirname, "code", dirName)
+
+    // 暂时只工作在code文件夹下
+    let workPath = path.join(__dirname, "code")
+    sandboxManager.createSandbox(workPath).then(sandbox => {
+        // terminal
+        sandbox.container.getCmdStream().then(stream => {
+            stream.on('data', (chunk) => {
+                socket.emit("data", chunk.toString());
+            });
+            socket.on("write", data => {
+                stream.write(data);
             })
-            // virtualFile
-            socket.on("disconnect", () => {
-                sandboxManager.deleteSandbox(sandbox.id);
-                deleteFolderRecursive(workPath);
-            })
-            const vfServerEventEmitter = new EventEmitter((event) => {
-                socket.emit("serverFileEvent", event)
-            })
-            let virtualFileServer = new VirtualFileServer(sandbox.workPath, vfServerEventEmitter, sandbox);
-            socket.on("virtualFileClientReady", () => {
-                virtualFileServer.start()
-            })
-            socket.on("clientFileEvent", (event) => {
-                virtualFileEvent.serverDefaultExecEvent(event, virtualFileServer);
-            })
-            socket.emit("virtualFileServerReady", { sandboxId: sandbox.id }); // 成功分配
-        }).catch(err => {
-            console.log(err);
         })
+        // virtualFile
+        socket.on("disconnect", () => {
+            sandboxManager.deleteSandbox(sandbox.id);
+            // temp
+            // 暂时固定文件夹
+            // deleteFolderRecursive(workPath);
+        })
+        const vfServerEventEmitter = new EventEmitter((event) => {
+            socket.emit("serverFileEvent", event)
+        })
+        let virtualFileServer = new VirtualFileServer(sandbox.workPath, vfServerEventEmitter, sandbox);
+        socket.on("virtualFileClientReady", () => {
+            virtualFileServer.start()
+        })
+        socket.on("clientFileEvent", (event) => {
+            virtualFileEvent.serverDefaultExecEvent(event, virtualFileServer);
+        })
+        socket.emit("virtualFileServerReady", { sandboxId: sandbox.id }); // 成功分配
+    }).catch(err => {
+        console.log(err);
     })
-});
+})
+
 
 server.listen(config.port, () => {
     console.log(`listening on ${config.port}`);
